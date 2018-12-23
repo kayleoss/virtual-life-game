b4eef6deb81b1ee162a4efe5650ced7da0830a18
Katie <katie_acting@live.ca>
2018-12-23 00:53:37 -0500

    add sql initialize

---
+++ db/db-initialize.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+    username varchar(100) not null,
+    password varchar(100) not null
+    -- add more fields for user schema
+);
